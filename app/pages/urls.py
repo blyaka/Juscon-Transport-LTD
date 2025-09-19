b351655618540ce198ae945de8dceaa01d379772
daryaztsv4 <daryaztsv4@gmail.com>
2025-09-19 12:37:54 +0300

    qqqq

--- app/pages/urls.py
+++ app/pages/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import HomePage
+from .views import HomePage, FormPage
 
 urlpatterns = [
     path('', HomePage, name='home'),
+    path('form/', FormPage, name='form'),
 ]
--- app/pages/views.py
+++ app/pages/views.py
@@ -4,6 +4,8 @@ from django.shortcuts import render
 def HomePage(request):
     return render(request, 'home.html')
 
+def FormPage(request):
+    return render(request, 'form.html')
 
 def Custom404(request, exception):
             return render(request, '404.html', status=404)
